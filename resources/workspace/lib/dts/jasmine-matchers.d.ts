2de942cf4ec56c1e3fc96c174e8f61251861ea5d
KLF <kfrencher@hotmail.com>
2024-08-31 16:25:32 -0400

    Updated to better support testing

--- resources/workspace/lib/dts/jasmine-matchers.d.ts
+++ resources/workspace/lib/dts/jasmine-matchers.d.ts
@@ -1,64 +1,87 @@
-/*
-Typings 2013 Bart van der Schoor
-
-TypeScript tests auto-extracted from jasmine-matchers unit test.
-
-Original jasmine-matchers license applies:
-
-Copyright (C) 2013, uxebu Inc.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-/// <reference types="jasmine" />
-
 declare namespace jasmine {
-    interface Matchers<T> {
-        // toBe
-        toBeArray(): boolean;
-        toBeString(): boolean;
-        toBeNonEmptyArray(): boolean;
-        toBeCloseToOneOf(values: any[], isCloseToFunction: (actual: number, expected: number) => boolean): boolean;
-        toBeInstanceOf(Constructor: Function): boolean;
-        toBeInRange(min: number, max: number): boolean;
-        toBeNan(): boolean;
-        toBeNumber(): boolean;
-        toBeOfType(type: string): boolean;
-        toBeOneOf(values: any[]): boolean;
+  interface Matchers<T> {
+    toBeAfter(otherDate: Date, expectationFailOutput?: any): boolean;
+    toBeArray(expectationFailOutput?: any): boolean;
+    toBeArrayOfBooleans(expectationFailOutput?: any): boolean;
+    toBeArrayOfNumbers(expectationFailOutput?: any): boolean;
+    toBeArrayOfObjects(expectationFailOutput?: any): boolean;
+    toBeArrayOfSize(size: number, expectationFailOutput?: any): boolean;
+    toBeArrayOfStrings(expectationFailOutput?: any): boolean;
+    toBeBefore(otherDate: Date, expectationFailOutput?: any): boolean;
+    toBeBoolean(expectationFailOutput?: any): boolean;
+    toBeCalculable(expectationFailOutput?: any): boolean;
+    toBeDate(expectationFailOutput?: any): boolean;
+    toBeEmptyArray(expectationFailOutput?: any): boolean;
+    toBeEmptyObject(expectationFailOutput?: any): boolean;
+    toBeEmptyString(expectationFailOutput?: any): boolean;
+    toBeEvenNumber(expectationFailOutput?: any): boolean;
+    toBeFalse(expectationFailOutput?: any): boolean;
+    toBeFunction(expectationFailOutput?: any): boolean;
+    toBeGreaterThanOrEqualTo(otherNumber: number, expectationFailOutput?: any): boolean;
+    toBeHtmlString(expectationFailOutput?: any): boolean;
+    toBeIso8601(expectationFailOutput?: any): boolean;
+    toBeJsonString(expectationFailOutput?: any): boolean;
+    toBeLessThanOrEqualTo(otherNumber: number, expectationFailOutput?: any): boolean;
+    toBeLongerThan(other: string, expectationFailOutput?: any): boolean;
+    toBeNear(number: number, epsilon: number, expectationFailOutput?: any): boolean;
+    toBeNonEmptyArray(expectationFailOutput?: any): boolean;
+    toBeNonEmptyObject(expectationFailOutput?: any): boolean;
+    toBeNonEmptyString(expectationFailOutput?: any): boolean;
+    toBeNumber(expectationFailOutput?: any): boolean;
+    toBeObject(expectationFailOutput?: any): boolean;
+    toBeOddNumber(expectationFailOutput?: any): boolean;
+    toBeRegExp(expectationFailOutput?: any): boolean;
+    toBeSameLengthAs(other: string, expectationFailOutput?: any): boolean;
+    toBeShorterThan(other: string, expectationFailOutput?: any): boolean;
+    toBeString(expectationFailOutput?: any): boolean;
+    toBeTrue(expectationFailOutput?: any): boolean;
+    toBeValidDate(expectationFailOutput?: any): boolean;
+    toBeWhitespace(expectationFailOutput?: any): boolean;
+    toBeWholeNumber(expectationFailOutput?: any): boolean;
+    toBeWithinRange(floor: number, ceiling: number, expectationFailOutput?: any): boolean;
 
-        // toContain
-        toContainOnce(value: any): boolean;
+    toEndWith(subString: string, expectationFailOutput?: any): boolean;
 
-        // toHave
-        toHaveBeenCalledXTimes(count: number): boolean;
-        toHaveLength(length: number): boolean;
-        toHaveOwnProperties(...names: string[]): boolean;
-        toHaveOwnPropertiesWithValues(obj: Object): boolean;
-        toHaveProperties(...names: string[]): boolean;
-        toHavePropertiesWithValues(obj: Object): boolean;
-        toExactlyHaveProperties(...names: string[]): boolean;
+    toHaveArray(key: string, expectationFailOutput?: any): boolean;
+    toHaveArrayOfBooleans(key: string, expectationFailOutput?: any): boolean;
+    toHaveArrayOfNumbers(key: string, expectationFailOutput?: any): boolean;
+    toHaveArrayOfObjects(key: string, expectationFailOutput?: any): boolean;
+    toHaveArrayOfSize(key: string, size?: number, expectationFailOutput?: any): boolean;
+    toHaveArrayOfStrings(key: string, expectationFailOutput?: any): boolean;
+    toHaveBoolean(key: string, expectationFailOutput?: any): boolean;
+    toHaveCalculable(key: string, expectationFailOutput?: any): boolean;
+    toHaveDate(key: string, expectationFailOutput?: any): boolean;
+    toHaveDateAfter(key: string, otherDate: Date, expectationFailOutput?: any): boolean;
+    toHaveDateBefore(key: string, otherDate: Date, expectationFailOutput?: any): boolean;
+    toHaveEmptyArray(key: string, expectationFailOutput?: any): boolean;
+    toHaveEmptyObject(key: string, expectationFailOutput?: any): boolean;
+    toHaveEmptyString(key: string, expectationFailOutput?: any): boolean;
+    toHaveEvenNumber(key: string, expectationFailOutput?: any): boolean;
+    toHaveFalse(key: string, expectationFailOutput?: any): boolean;
+    toHaveHtmlString(key: string, expectationFailOutput?: any): boolean;
+    toHaveIso8601(key: string, expectationFailOutput?: any): boolean;
+    toHaveJsonString(key: string, expectationFailOutput?: any): boolean;
+    toHaveMember(key: string, expectationFailOutput?: any): boolean;
+    toHaveMethod(key: string, expectationFailOutput?: any): boolean;
+    toHaveNonEmptyArray(key: string, expectationFailOutput?: any): boolean;
+    toHaveNonEmptyObject(key: string, expectationFailOutput?: any): boolean;
+    toHaveNonEmptyString(key: string, expectationFailOutput?: any): boolean;
+    toHaveNumber(key: string, expectationFailOutput?: any): boolean;
+    toHaveNumberWithinRange(key: string, floor: number, ceiling: number, expectationFailOutput?: any): boolean;
+    toHaveObject(key: string, expectationFailOutput?: any): boolean;
+    toHaveOddNumber(key: string, expectationFailOutput?: any): boolean;
+    toHaveString(key: string, expectationFailOutput?: any): boolean;
+    toHaveStringLongerThan(key: string, other: string, expectationFailOutput?: any): boolean;
+    toHaveStringSameLengthAs(key: string, other: string, expectationFailOutput?: any): boolean;
+    toHaveStringShorterThan(key: string, other: string, expectationFailOutput?: any): boolean;
+    toHaveTrue(key: string, expectationFailOutput?: any): boolean;
+    toHaveUndefined(key: string, expectationFailOutput?: any): boolean;
+    toHaveWhitespaceString(key: string, expectationFailOutput?: any): boolean;
+    toHaveWholeNumber(key: string, expectationFailOutput?: any): boolean;
 
-        // toStartEndWith
-        toStartWith(value: any): boolean;
-        toStartWith(value: any[]): boolean;
+    toStartWith(subString: string, expectationFailOutput?: any): boolean;
 
-        toEndWith(value: any): boolean;
-        toEndWith(values: any[]): boolean;
-
-        toEachStartWith(searchString: string): boolean;
-        toSomeStartWith(searchString: string): boolean;
-
-        toEachEndWith(searchString: string): boolean;
-        toSomeEndWith(searchString: string): boolean;
-
-        toStartWithEither(...searchString: any[]): boolean;
-
-        // toThrow
-        toThrowInstanceOf(klass: Function): boolean;
-        toThrowStartsWith(text: string): boolean;
-    }
+    toThrowAnyError(expectationFailOutput?: any): boolean;
+    toThrowErrorOfType(type: string, expectationFailOutput?: any): boolean;
+  }
 }
